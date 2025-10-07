7e9fe73197f651bc065dab06f738a5378cd1d037
zattomato <izzathafizuddin@outlook.com>
2025-10-07 18:02:15 +0800

    base url use env variable

--- src/const/index.js
+++ src/const/index.js
@@ -1,4 +1,1 @@
-export const baseUrl = "https://backend-service-marketplace.vercel.app/api";
-
-// https://backend-service-marketplace.vercel.app/api
-// http://localhost:3000/api
+export const baseUrl = process.env.BACKEND_URL;
